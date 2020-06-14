9f0a2b509462047ea9915741d210005565e62912
kanishk0210 <kanishkchoudhary02@gmail.com>
2020-06-14 20:50:23 +0530

    navigation

---
+++ components/MainComponent.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Menu from './MenuComponent';
+import { DISHES } from '../shared/dishes';
+import DishDetail from './DishDetailComponent';
+import { View, Platform } from 'react-native';
+import { createStackNavigator} from 'react-navigation';
+
+const MenuNavigator = createStackNavigator({
+    Menu: { screen: Menu },
+    DishDetail: { screen: DishDetail }
+}, {
+    initialRouteName: 'Menu',
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor: '#512DAB'
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            color: '#fff'
+        }
+    }
+});
+
+class Main extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES,
+            selectedDish: null
+        };
+    }
+
+    onDishSelect(dishId) {
+        this.setState({ selectedDish: dishId });
+    }
+
+    render() {
+        return(
+            <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }} >
+                <MenuNavigator />
+            </View>                            
+        );
+    }
+}
+
+export default Main;
